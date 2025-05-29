c3c545c5651be7bc5e614a7c1e7bb018f1b2dfcf
ManMan3392 <2469739462@qq.com>
2025-05-30 00:47:26 +0800

    react状态管理

--- 作业/第4次作业-2024211996-张梦芸/lv3.js
+++ 作业/第4次作业-2024211996-张梦芸/lv3.js
@@ -1,1 +1,13 @@
-console.log((x => x * 2)(1));
+const arr = [1, 2, 3, 4]
+const iter = arr[Symbol.iterator]()
+for (let num of arr) {
+    console.log(num)
+    if (num >= 2) {
+        console.log('>= 2, quit')
+        break
+    }
+}
+
+for (let num of iter) {
+    console.log(num)
+}
